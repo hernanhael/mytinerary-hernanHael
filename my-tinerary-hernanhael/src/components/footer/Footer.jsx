8ce1b0a2116a1412a2e6bb2d2a9c0764701b437f
brain_damage <hernanhael@gmail.com>
2023-08-07 22:00:24 -0300

    style: fix main & footer

--- my-tinerary-hernanhael/src/components/footer/Footer.jsx
+++ my-tinerary-hernanhael/src/components/footer/Footer.jsx
@@ -2,5 +2,12 @@ import React from "react";
 import "./footer.css";
 
 export const Footer = () => {
-  return <div className="app-footer">Footer</div>;
+  return <div className="app-footer">
+    <div className="description">lorem
+    </div>
+    <div className="ubication">
+      map
+    </div>
+    <div className="social-media">Icons</div>
+  </div>;
 };
